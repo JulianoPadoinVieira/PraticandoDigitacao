b92b3dcfa9726d79bb0430b9ae37e8c91c457c29
proprietyofkurd@hotmail.com <proprietyofkurd@hotmail.com>
2020-03-08 20:06:25 -0300

    Versão 1

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace PraticandoDigitacao {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
